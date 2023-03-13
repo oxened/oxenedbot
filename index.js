1b75cffe623809ae259fe4ec1e6a1af2af98407f
oxenn <tvoya@mama.ua>
2023-03-13 18:07:04 +0300

    ецлипце эвал апдейт 😨

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { token, clientid } = require("./config.json")
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const fs = require('node:fs');
 const path = require('node:path');
-const util = require('util');
+const { inspect }  = require('util');
 client.commands = new Collection();
 const rest = new REST({ version: '10' }).setToken(token);
 
@@ -27,7 +27,7 @@ const eval_ = new SlashCommandBuilder()
 const commands = [ban, eval_]
 
 client.on('ready', async () => {
-	console.log(client.user.tag)
+	console.log(client.user.tag);
 
 	client.user.setPresence({
 		status: 'idle',
@@ -37,38 +37,42 @@ client.on('ready', async () => {
 		}]
 	})
 	try {
-		console.log("хахвхвха ну типо э")
+		console.log("хахвхвха ну типо э");
 		await rest.put(Routes.applicationCommands(clientid), { body: commands })
-		console.log("всё.")
-	} catch(error) { console.error(error) }
+		console.log("всё.");
+	} catch(error) { console.error(error); }
 })
 
 
 client.on('interactionCreate', async (interaction, message, user) => {
-	if(!interaction.isChatInputCommand()) return
+	if(!interaction.isChatInputCommand()) return;
 
 	if(interaction.commandName == 'ban') {
 		const loshped = interaction.options.getUser('user')
 		if(loshped == '530377558508699659') { 
-			await interaction.reply("Вы не можете забанить создателя сервера.")
+			await interaction.reply("Вы не можете забанить создателя сервера.");
 		} else if(loshped == '1023594263176564878') {
-			await interaction.reply("маму свою забань мудила")
+			await interaction.reply("маму свою забань мудила");
 		}
-		await interaction.reply(`Пользователь ${interaction.options.getUser('user')} был забанен. <:ralsei_wtf:1084577747063545886> <a:AU_Z:1067782077065478266>`)
+		await interaction.reply(`Пользователь ${interaction.options.getUser('user')} был забанен. <:ralsei_wtf:1084577747063545886> <a:AU_Z:1067782077065478266>`);
 	}
 	if(interaction.commandName == 'eval') {
 		if(interaction.user.id != 530377558508699659) {
-			await interaction.reply("бёже бёт")
+			await interaction.reply("бёже бёт");
+			return;
 		}
-		try { 
-			const res = await eval(interaction.options.getString('command')) 
-			await interaction.reply(`⚙️ ТЕХНИКА РАБОТАЕТ ✅ ${res}`)
-		} catch(error) { console.log(error); await interaction.reply(`⚙️ ТЕХНИКА НЕ РАБОТАЕТ ❎ ${error}`) }
+		let res;
+		await interaction.deferReply();
+
+		try {
+			res = await eval(interaction.options.getString('command'));
+		} catch(error) { res = error }
+		await interaction.editReply(`\`\`\`js\n${inspect(res, { depth: 0 }).slice(0, 1900)}\n\`\`\``);
 	}
 })
 
 process.on('uncaughtException', (err) => {
-	console.log(err)
+	console.log(err);
 })
 
-client.login(token)
+client.login(token);
