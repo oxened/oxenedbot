8eb8d0969cec23d68a927f9a5747a0c1e45cdc29
oxenn <tvoya@mama.ua>
2023-03-13 16:48:15 +0300

    ецлипце бёже бёт апдейт

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const { token, clientid } = require("./config.json")
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const fs = require('node:fs');
 const path = require('node:path');
+const util = require('util');
 client.commands = new Collection();
 const rest = new REST({ version: '10' }).setToken(token);
 
-const ban = new SlashCommandBuilder().setName("ban").setDescription("да")
-const commands = [ban]
+const ban = new SlashCommandBuilder()
+	.setName("ban")
+	.setDescription("Забанить.")
+	.addUserOption(option => option
+		.setName("user")
+		.setDescription("Укажите пользователя.")
+		.setRequired(true)
+	)
+
+const eval_ = new SlashCommandBuilder()
+	.setName("eval")
+	.setDescription("eval")
+	.addStringOption(option => option
+		.setName("command")
+		.setDescription("...")
+		.setRequired(true)
+	)
+const commands = [ban, eval_]
 
 client.on('ready', async () => {
 	console.log(client.user.tag)
@@ -27,11 +44,31 @@ client.on('ready', async () => {
 })
 
 
-client.on('interactionCreate', async (interaction) => {
+client.on('interactionCreate', async (interaction, message, user) => {
 	if(!interaction.isChatInputCommand()) return
 
 	if(interaction.commandName == 'ban') {
-		await interaction.reply("скоро")
+		const loshped = interaction.options.getUser('user')
+		if(loshped == '530377558508699659') { 
+			await interaction.reply("Вы не можете забанить создателя сервера.")
+		} else if(loshped == '1023594263176564878') {
+			await interaction.reply("маму свою забань мудила")
+		}
+		await interaction.reply(`Пользователь ${interaction.options.getUser('user')} был забанен. <:ralsei_wtf:1084577747063545886> <a:AU_Z:1067782077065478266>`)
+	}
+	if(interaction.commandName == 'eval') {
+		if(interaction.user.id != 530377558508699659) {
+			await interaction.reply("бёже бёт")
+		}
+		try { 
+			const res = await eval(interaction.options.getString('command')) 
+			await interaction.reply(`⚙️ ТЕХНИКА РАБОТАЕТ ✅ ${res}`)
+		} catch(error) { console.log(error); await interaction.reply(`⚙️ ТЕХНИКА НЕ РАБОТАЕТ ❎ ${error}`) }
 	}
 })
+
+process.on('uncaughtException', (err) => {
+	console.log(err)
+})
+
 client.login(token)
