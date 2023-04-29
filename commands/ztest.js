4e3f6906e52bf7156152783e0596e8da828daaee
я <g>
2023-04-29 18:41:13 +0300

    ну э

---
+++ commands/ztest.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ztest")
+    .setDescription("ок т"),
+  async execute(interaction) {
+    await interaction.reply("лошпед")
+  },
+}
