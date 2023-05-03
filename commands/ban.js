296ef327708f1ed55693357f04e7619317022af5
zlinuxc <99759342+zlinuxc@users.noreply.github.com>
2023-05-03 21:58:10 +0300

    switch :trlo:

--- commands/ban.js
+++ commands/ban.js
@@ -10,8 +10,21 @@ module.exports = {
       .setRequired(true)
   ),
   async execute (interaction) {
-    let loshped = interaction.options.getUser("user")
-    let msg = (loshped == "530377558508699659") ? "Вы не можете забанить создателя сервера." : (loshped == clientid) ? "Маму свою забань, мудила." : (loshped == interaction.user.id) ? "Самокритично." : `${loshped} был забанен. <:ralsei_wtf:1103379157620097205> <a:Z_:1099932029627400192>`
+    let loshped = interaction.options.getUser("user");
+    let msg;
+    switch (loshped.id) {
+        case "530377558508699659":
+            msg = "Вы не можете забанить создателя сервера.";
+            break;
+        case clientid:
+            msg = "Маму свою забань, мудила.";
+            break
+        case interaction.user.id:
+            msg = "Самокритично.";
+            break;
+        default:
+            msg = `${loshped} был забанен. <:ralsei_wtf:1103379157620097205> <a:Z_:1099932029627400192>`;
+    }
     interaction.reply(msg)
   }
 }
