5721b60046d2c0f371a4081bfb68a7625356a7c2
лошпед <61601576+oxenn@users.noreply.github.com>
2023-05-08 20:27:49 +0300

    (((((((((((((((((

--- index.js
+++ index.js
@@ -1,65 +1,82 @@
-const { Client, GatewayIntentBits, Collection, Events, REST, Routes, ActivityType, SlashCommandBuilder } = require('discord.js')
-const { token, clientid } = require("./config.json")
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  Events,
+  REST,
+  ActivityType,
+  SlashCommandBuilder,
+} = require('discord.js');
+const { token, clientid, bingc } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], presence: { status: "idle", activities: [{ name: "😭", type: ActivityType.Playing}]}});
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  presence: {
+    status: 'idle',
+    activities: [{ name: '😭', type: ActivityType.Playing }],
+  },
+});
 
 const fs = require('node:fs');
 const path = require('node:path');
-const { inspect }  = require('util');
+const { inspect } = require('util');
 client.commands = new Collection();
 const rest = new REST({ version: '10' }).setToken(token);
 
-const commands = []
-
-
-let loshara = ["1043211191067103263", "530377558508699659"]
+let loshara = ['1043211191067103263', '530377558508699659'];
 
 client.on('ready', async () => {
   console.log(client.user.tag);
 
-    const commandsPath = path.join(__dirname, 'commands');
-    const commansFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  const commandsPath = path.join(__dirname, 'commands');
+  const commansFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
 
   for (const file of commansFiles) {
-    const filePath = path.join(commandsPath, file)
-    const command = require(filePath)
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
     if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command)
-        commands.push(command.data.toJSON())
+      client.commands.set(command.data.name, command);
+      commands.push(command.data.toJSON());
     } else {
-        console.log(`в ${filePath} нет "data" или "execute"`)
+      console.log(`в ${filePath} нет "data" или "execute"`);
     }
   }
 
-	try {
-		console.log("обновление команд");
-        await client.application.commands.set(commands)
-		console.log("обновление команд завершено :wtf2:");
-	} catch(error) { console.error(error); }
-})
-
+  try {
+    console.log('обновление команд');
+    await client.application.commands.set(commands);
+    console.log('обновление команд завершено :wtf2:');
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 client.on('interactionCreate', async (interaction, message, user) => {
-	if(!interaction.isChatInputCommand()) return;
-    const cmd = interaction.client.commands.get(interaction.commandName);
-    try {
-        await cmd.execute(interaction)
-    } catch(error) {
-        interaction.reply("Егор отшибка 😭");
-        console.log(error)
-    }
-
-})
+  if (!interaction.isChatInputCommand()) return;
+  const cmd = interaction.client.commands.get(interaction.commandName);
+  try {
+    await cmd.execute(interaction);
+  } catch (error) {
+    interaction.reply('Егор отшибка 😭');
+    console.log(error);
+  }
+});
 
 process.on('uncaughtException', (err) => {
-	console.error(err);
+  console.error(err);
 });
 
 client.on('messageCreate', (message) => {
-	let msg = message.content
-	if (msg.includes(`<@${clientid}>`)) {
-		message.reply("Больше никакого <a:AU_Z:1067782077065478266>.");
-	}
-})
+  let msg = message.content;
+  if (msg.includes(`<@${clientid}>`)) {
+    message.reply('Больше никакого <a:AU_Z:1067782077065478266>.');
+  }
+});
 
 client.login(token);
