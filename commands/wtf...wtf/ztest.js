3d1df61235c2d58353c385038f4cbb54cc0fa9ff
лошпед <61601576+oxenn@users.noreply.github.com>
2023-05-09 16:16:35 +0300

    .

--- commands/wtf...wtf/ztest.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('ztest').setDescription('ок т'),
-    async execute(interaction) {
-        await interaction.reply('лошпед');
-    },
-};
