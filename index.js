b9c78b64911cc2afdf21edc81f4564cd96fe98d9
лошпед <61601576+oxenn@users.noreply.github.com>
2023-05-10 20:05:02 +0300

    .

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
     ],
     presence: {
         status: 'idle',
