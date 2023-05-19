9f7dec19d3b1a0ee52f12c15a3bc0692b4987181
zlinuxc <99759342+zlinuxc@users.noreply.github.com>
2023-05-19 18:00:34 +0300

    поаап

--- commands/dev/bash.js
+++ commands/dev/bash.js
@@ -1,23 +1,22 @@
-const { SlashCommandBuilder } = require("discord.js")
-const { execSync } = require("node:child_process")
-const loshara = ['1043211191067103263', '530377558508699659']
+const {SlashCommandBuilder} = require("discord.js");
+const {execSync} = require("node:child_process");
+const loshara = ["1043211191067103263", "530377558508699659"];
 // НЕ ДОДЕЛАНО
 module.exports = {
-  data: new SlashCommandBuilder()
-  .setName("bash")
-  .setDescription("?")
-  .addStringOption(
-    option => option
-    .setName("command")
-    .setDescription("?")
-    .setRequired(true)
-  ),
-  async execute(interaction) {
-  if (!loshara.includes(interaction.user.id)) {
-  interaction.reply("🙄");
-  return;
-}
-  const res = execSync(interaction.options.getString("command")).toString().slice(0, 1900);
-  interaction.reply(` \`\`\`${res}\`\`\` `)
-}
-}
+    data: new SlashCommandBuilder()
+        .setName("bash")
+        .setDescription("?")
+        .addStringOption((option) =>
+            option.setName("command").setDescription("?").setRequired(true)
+        ),
+    async execute(interaction) {
+        if (!loshara.includes(interaction.user.id)) {
+            interaction.reply("🙄");
+            return;
+        }
+        const res = execSync(interaction.options.getString("command"))
+            .toString("ucs2")
+            .slice(0, 1900);
+        interaction.reply(` \`\`\`${res}\`\`\` `);
+    },
+};
